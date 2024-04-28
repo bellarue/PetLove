2557bc3dd8513d4b787ede70f7b3e686a4fb262b
Phi Do <76242187+PhiHDo@users.noreply.github.com>
2024-04-28 14:05:18 -0700

    Delete duplicate src directory

--- src/Components/Settings/ManagePets.js
+++
@@ -1,9 +0,0 @@
-import React, {Fragment, useState, useEffect} from 'react';
-import { Box } from '@mui/material';
-import Typography from '@mui/material/Typography';
-
-export default function ManagePets(props) {
-    return <Typography>
-        manage pets
-    </Typography>
-}
